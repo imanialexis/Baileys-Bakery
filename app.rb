5f1fc7a65e87ccd9d4831be3d66c5b0a1f294fb5
imani alexis <alexis.bailey37@gmail.com>
2018-08-05 17:48:01 -0400

    all three classes added with corresponding erbs

--- app.rb
+++ app.rb
@@ -7,9 +7,39 @@ cookie_1 = Cookie.new('Chocolate Chip','A description','images/chocolate_chip','
 cookie_2 = Cookie.new('White Chocolate','A description','images/chocolate_chip','$5')
 cookie_3 = Cookie.new('Oatmeal','A description','images/chocolate_chip','$6')
 
+
+
 get "/" do
+   "I AM THE GREATEST"
+end
+
+get "/cookies" do
     @cookie_1 = cookie_1
     @cookie_2 = cookie_2
     @cookie_3 = cookie_3
-    erb :test
+    
+    erb :cookie
+end
+
+
+cake_1 = Cake.new('Chocolate', 'A description','images/chocolate_chip','$10')
+cake_2 = Cake.new('Ice Cream', 'A description','images/chocolate_chip','$12')
+cake_3 = Cake.new('Cheesecake', 'A description','images/chocolate_chip','$14')
+
+get "/cakes" do
+    @cake_1 = cake_1
+    @cake_2 = cake_2
+    @cake_3 = cake_3
+    erb :cake
+end
+
+muffin_1 = Muffin.new('Blueberry','A description','images/chocolate_chip','$4')
+muffin_2 = Muffin.new('Chocolate','A description','images/chocolate_chip','$4')
+muffin_3 = Muffin.new('Strawberry', 'A description','images/chocolate_chip','$14')
+
+get "/muffins" do
+    @muffin_1 = muffin_1
+    @muffin_2 = muffin_2
+    @muffin_3 = muffin_3
+    erb :muffin
 end
--- myclasses.rb
+++ myclasses.rb
@@ -8,6 +8,26 @@ class Cookie
     end
 end
 
+class Cake
+    attr_accessor :name,:describe,:photo,:price
+    def initialize(name,describe,photo,price)
+        @name = name
+        @describe = describe
+        @photo = photo
+        @price = price
+    end
+end
+
+
+class Muffin
+    attr_accessor :name,:describe,:photo,:price
+    def initialize(name,describe,photo,price)
+        @name = name
+        @describe = describe
+        @photo = photo
+        @price = price
+    end
+end
 
 
 
