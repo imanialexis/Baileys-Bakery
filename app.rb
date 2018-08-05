efeadfb74713884864e077cf91dcf25d5356b6f0
imani alexis <alexis.bailey37@gmail.com>
2018-08-05 15:32:44 -0400

    added class and test erb to check if everything is rendering correctly

--- app.rb
+++ app.rb
@@ -2,4 +2,14 @@ require 'sinatra'
 require 'colorize'
 require './myclasses.rb'
 
+# :name, :describe, :photo , :price
+cookie_1 = Cookie.new('Chocolate Chip','A description','images/chocolate_chip','$4')
+cookie_2 = Cookie.new('White Chocolate','A description','images/chocolate_chip','$5')
+cookie_3 = Cookie.new('Oatmeal','A description','images/chocolate_chip','$6')
 
+get "/" do
+    @cookie_1 = cookie_1
+    @cookie_2 = cookie_2
+    @cookie_3 = cookie_3
+    erb :test
+end
--- myclasses.rb
+++ myclasses.rb
@@ -1,3 +1,18 @@
+class Cookie
+    attr_accessor :name,:describe,:photo,:price
+    def initialize(name,describe,photo,price)
+        @name = name
+        @describe = describe
+        @photo = photo
+        @price = price
+    end
+end
+
+
+
+
+
+
 
 
 
