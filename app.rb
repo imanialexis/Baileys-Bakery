61f073bbccd44495944864bcc21c4a2b384c9afb
imani alexis <alexis.bailey37@gmail.com>
2018-08-07 15:56:42 -0400

    added about us sections, styles, images edited classes

--- app.rb
+++ app.rb
@@ -3,10 +3,11 @@ require 'colorize'
 require './myclasses.rb'
 require 'sinatra/reloader'
 
+
 # :name, :describe, :photo , :price
-cookie_1 = Cookie.new('Chocolate Chip','A description','images/chocolate_chip','$4')
-cookie_2 = Cookie.new('White Chocolate','A description','images/chocolate_chip','$5')
-cookie_3 = Cookie.new('Oatmeal','A description','images/chocolate_chip','$6')
+cookie_1 = Cookie.new('Chocolate Chip','Magically Delicious','img/cookie.jpg','$4')
+cookie_2 = Cookie.new('White Chocolate','Magically Delicious','img/whitecookie.jpeg','$4')
+cookie_3 = Cookie.new('Oatmeal','Magically Delicious','img/oatcookie.jpg','$5')
 
 
 
@@ -14,9 +15,9 @@ get "/" do
    erb :home
 end
 
-get "/home" do
-    erb :home
- end
+# get "/home" do
+#     erb :home
+#  end
 
 get "/cookies" do
     @cookie_1 = cookie_1
@@ -27,25 +28,27 @@ get "/cookies" do
 end
 
 
-cake_1 = Cake.new('Chocolate', 'A description','img/placeholder.jpg','$10')
-cake_2 = Cake.new('Ice Cream', 'A description','img/placeholder.jpg','$12')
-cake_3 = Cake.new('Cheesecake', 'A description','img/placeholder.jpg','$14')
+cake_1 = Cake.new('Carrot Cake', 'Magically Delicious','img/cake.jpg','$22')
+cake_2 = Cake.new('Chocolate Cake', 'Magically Delicious','img/chococake.png','$18')
+cake_3 = Cake.new('Red Velvet', 'Magically Delicious','img/velvet.jpg','$20')
 
 get "/cakes" do
     @cake_1 = cake_1
     @cake_2 = cake_2
     @cake_3 = cake_3
+    @all_cakes = [@cake_1,@cake_2,@cake_3]
     erb :cake
 end
 
-muffin_1 = Muffin.new('Blueberry','A description','images/chocolate_chip','$4')
-muffin_2 = Muffin.new('Chocolate','A description','images/chocolate_chip','$4')
-muffin_3 = Muffin.new('Strawberry', 'A description','images/chocolate_chip','$14')
+muffin_1 = Muffin.new('Blueberry','Magically Delicious','img/bmuffin.jpg','$5')
+muffin_2 = Muffin.new('Chocolate',' Magically Delicious','img/chocmuffin.jpg','$3')
+muffin_3 = Muffin.new('Strawberry', 'Magically Delicious','img/strawmuffin.jpg','$4')
 
 get "/muffins" do
     @muffin_1 = muffin_1
     @muffin_2 = muffin_2
     @muffin_3 = muffin_3
+    @all_muffins = [@muffin_1,@muffin_2,@muffin_3]
     erb :muffin
 end
 
